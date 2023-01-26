ce19a2e24f6575b4e047413331cd12d8373657dc
francesperez <frances.perez141@gmail.com>
2023-01-25 19:39:16 -0600

    Svelete todo list

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<svelte:head>
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+</svelte:head>
+
+<script>
+    let toDoList = []; //array of ToDos
+
+    let textInput = "";
+
+    function addToDo(){
+        toDoList = [...toDoList, {content: textInput, editing: false, checked: false}]
+    }
+
+    function setEditing(i, isEditing) {
+        toDoList[i].editing = isEditing; //true or false
+    }
+function deleteTodo(i) {
+    toDoList.splice(i, 1);
+    toDoList = toDoList; //telling svelte that we need to delete from the list (from itself)
+}
+
+</script>
+
+<div style="margin: 0 auto; padding: 20px; mdith: 700px;">
+    <h2 style="text-align: center;">To Do List</h2>
+    <p>Enter you todo here</p>
+    <div style="display:flex;">
+        <input type="text" bind:value={textInput}>
+        <button style="width: 200px;" on:click={addToDo}>Add</button>
+    </div>
+</div>
+
+
+{#each toDoList as toDo, i}   <!-- this is going to go through every single element in the toDoList and to get access to the element, we can go to ToDo. It is pretty much saying go through every single element in the index and appending that to the html. We can access that object through the variable toDo! -->
+
+
+
+<div style="display: flex; align-items: baseline; width: 700px; margin: 0 auto;">
+    {#if toDo.editing}
+        <input type="text" bind:value={toDo.content}>
+    {:else}
+        <input type="checkbox" bind:checked={toDo.checked}> <!--This is taking the checked attribute of input and bind it to the variable in our javascript code, such as toDo.checked.-->
+        <h4 style="flex-grow: 1;">{toDo.content}</h4>
+    {/if}
+    <div style="display:flex;">
+        {#if toDo.editing}
+            <button on:click={() => setEditing(i, false)}>Save</button>
+            {:else}
+            <button on:click={() => setEditing(i, true)}>Edit</button>
+        {/if}
+        <button on:click={() => deleteTodo(i)} >Delete</button>
+    </div>
+</div>
+
+{/each}
